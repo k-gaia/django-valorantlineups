328426ead0d8a0c442a8a6fe6b4423b1bf67e561
k-gaia <kevjnr@live.co.uk>
2022-01-22 21:02:12 +0000

    database testing

--- valorantlineups/lineups/models.py
+++ valorantlineups/lineups/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+# Choice Variables
+con = "CONTROLLER"
+duel = "DUELIST"
+init = "INITIATOR"
+sent = "SENTINEL"
+
+AGENTCLASS_CHOICES = (
+
+    (con, "controller"),
+    (duel, "duelist"),
+    (init, "initiator"),
+    (sent, "sentinel")
+
+)
+
 # Create your models here.
+
+class Agent(models.Model):
+    name = models.CharField(max_length=64)
+    agentClass = models.CharField(max_length=10, choices=AGENTCLASS_CHOICES, default='CONTROLLER')
+
+    def __str__(self):
+        return self.name + " - " + self.agentClass
