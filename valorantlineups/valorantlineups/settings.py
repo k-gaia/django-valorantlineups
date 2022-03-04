ea4518f1aa41072711b0a154e94710c98c60fc11
k-gaia <kevjnr@live.co.uk>
2022-03-04 19:53:47 +0000

    hosting setup #2

--- valorantlineups/valorantlineups/settings.py
+++ valorantlineups/valorantlineups/settings.py
@@ -143,7 +143,7 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
